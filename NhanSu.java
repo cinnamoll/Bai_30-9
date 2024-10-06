114c2d2141be949b5d42b951057c4561d76cca46
cinnamoll <danganhque@gmail.com>
2024-10-06 22:20:56 +0700

    Change NhanSu and add uml

--- NhanSu.java
+++ NhanSu.java
@@ -1,6 +1,6 @@
 public class NhanSu {
     private String name;
-    private String year;
+    private int year;
 
     public NhanSu(String name, int year) {
         this.name = name;
@@ -15,32 +15,15 @@ public class NhanSu {
         this.name = name;
     }
 
-    public void setYear(String year) {
+    public void setYear(int year) {
         this.year = year;
     }
 
-    public String getYear() {
+    public int getYear() {
         return year;
     }
 
     public String getName() {
         return name;
-    }
-
-    public class Student extends NhanSu {
-        private String mssv;
-
-        public Student(String name, String year, String mssv) {
-            super(name, year);
-            this.mssv = mssv;
-        }
-
-        public void setMssv(String mssv) {
-            this.mssv = mssv;
-        }
-
-        public String getMssv() {
-            return mssv;
-        }
     }
 }
